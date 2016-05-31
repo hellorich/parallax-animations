62844eb5ab271ef8a7c705398eca81569f3c7d16
Rich C <rich.cookson@gmail.com>
2016-05-31 22:34:33 +0100

    Parallax scripts

--- src/js/modules/parallaxController.js
+++ src/js/modules/parallaxController.js
@@ -7,13 +7,64 @@ define('parallaxController', [
   'animation.gsap'], function($, TweenMax, ScrollMagic, jqueryScrollMagic, debugAddIndicators, animationGsap) {
 
   'use strict';
-  
-  var privateMethod = function (message) {
-    console.log('Parallax Controller OK!');
+
+
+
+  var addToScene = function ( controller, scene, timeline ) {
+
+    new ScrollMagic.Scene({
+      duration: '200%',
+      triggerElement: scene
+    })
+    .setTween( timeline )
+    .addTo( controller );
+    
   };
 
-  var init = function (text) {
-    privateMethod(text);
+
+
+  var parallax = function ( controller ) {
+
+    var $scene = $('.animation');
+
+    $scene.each( function() {
+
+      var $animation = $(this),
+          $animationCaptions = $animation.find('.animation-captions'),
+          $animationGraphics = $animation.find('.animation-graphics');
+
+      var timeline = new TimelineMax().add([
+
+        TweenMax.from( $animationCaptions, 1, { 
+          y: '+60%', 
+          ease: Power0.easeNone 
+        }),
+
+        TweenMax.from( $animationGraphics, 1, { 
+          y: '+30%', 
+          ease: Power0.easeNone 
+        })
+
+      ]);
+
+      addToScene( controller, $animation, timeline );
+
+    })
+
+  };
+
+
+
+  var init = function () {
+
+    var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onEnter'
+      }
+    });
+
+    parallax( controller );
+
   };
   
   return {
